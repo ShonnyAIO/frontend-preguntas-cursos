a62e899a74caed2a845ae18ab29d5b14ff5e6e8e
Jonathan Torres <exodiakah@gmail.com>
2024-01-08 09:29:57 -0400

    Fixed URL ENV

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    apiURL : 'https://backend-preguntas-cursos.jonathantorres6.repl.co'
+    apiURL : 'https://3a9ea278-4ee2-47b1-99a9-cc01019a2c61-00-a59xh3wgij7h.spock.replit.dev'
 };
